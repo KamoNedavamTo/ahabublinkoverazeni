280f3fb378e43dfa39090a482840f743ab9be099
Jáchym Vlček <jachym.vlcek@orbiszlin.cz>
2024-01-19 13:31:02 +0100

    už to bublinkuje

--- main.c
+++ main.c
@@ -4,16 +4,20 @@ int main() {
 
     int nums[] = {1, 3, 5, 2, 1};
 
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < i - 1; ++j) {
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5 - i- 1; ++j) {
             if(nums[j] > nums[j + 1]){
 
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
 
             }
         }
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        printf("%d", nums[i]);
     }
 
     return 0;
