f7441b503b9213a5cc34f0409a5ee8194bd7cfc6
Jáchym Vlček <jachym.vlcek@orbiszlin.cz>
2024-01-26 13:50:45 +0100

    lepší bubble sort

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 
+/**
+ *sorting array by bubble sort
+ *
+ * @param arr array of ints
+ * @param size size of array
+ * @param order bool 1 = ASC, 0 = DESC
+ */
+void bubbleSort (int arr[], int size, int order) {
+
+    if (!(order == 0 || order == 1)) {
+        printf("Order arg must 1 or 0!!!!\n");
+        return;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if(order == 0 && arr[j] < arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr [j +1];
+                arr[j + 1] = tmp;
+            } else if (order == 1 && arr[j] > arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr [j +1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
+
 int main() {
 
     int nums[] = {1, 3, 5, 2, 1};
-
+/*
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5 - i- 1; ++j) {
             if(nums[j] > nums[j + 1]){
@@ -15,9 +44,18 @@ int main() {
             }
         }
     }
+*/
+    for (int i = 0; i < 5; ++i) {
+        printf("%d ", nums[i]);
+    }
+
+    printf("\n");
+
+    bubbleSort(nums, 5, 0);
+
 
     for (int i = 0; i < 5; ++i) {
-        printf("%d", nums[i]);
+        printf("%d ", nums[i]);
     }
 
     return 0;
